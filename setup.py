80bed366f756350c2c52cc0ac9c85a79ea302aab
Nick Zarczynski <nick@nickzarr.com>
2015-08-06 03:42:35 -0700

    Version bump and release (at least to myself :) )

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
   setup(
     name='unb-cli',
-    version='0.0.2',
+    version='0.0.3',
     description='Command line utilities for UNB project development.',
     author='Nick Zarczynski',
     author_email='nick@unb.services',
