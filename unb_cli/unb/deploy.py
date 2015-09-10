ce91d4d2a6699819243570754369208d746ea828
Nick Zarczynski <nick@nickzarr.com>
2015-09-10 08:51:41 -0700

    Improve deployment commands

--- unb_cli/unb/deploy.py
+++ unb_cli/unb/deploy.py
@@ -10,19 +10,6 @@ group = Group(
 )
 
 
-@group.command('staging')
-def staging():
-  """Deploy to the default Heroku remote.
-
-  This assumes that a git remote named "heroku" exists and points to the
-  staging environment.
-  """
-  # Deploy to staging environment
-  subprocess.call(['git', 'push', 'heroku', 'master'])
-  subprocess.call(
-    ['heroku', 'run', './manage.py', 'migrate'])
-
-
 @group.command('run')
 @arg('args', nargs=argparse.REMAINDER,
      help="Arguments to pass to the manage.py command.")
@@ -30,3 +17,25 @@ def run(args):
   """Run a command using `heroku run ./manage.py` on the default Heroku app."""
   base = ['heroku', 'run', './manage.py']
   subprocess.call(base + args)
+
+
+@group.command('migrate')
+def migrate():
+  """Run migrations on the default Heroku app."""
+  subprocess.call(['heroku', 'run', './manage.py', 'migrate'])
+
+
+@group.command('shell')
+def shell():
+  """Run a Django shell on the default Heroku app."""
+  subprocess.call(['heroku', 'run', './manage.py', 'shell_plus'])
+
+
+@group.command('heroku')
+def heroku():
+  """Deploy to the Heroku app associated with a git remote named `heroku`.
+
+  This assumes that a git remote named "heroku" exists.
+  """
+  subprocess.call(['git', 'push', 'heroku', 'master'])
+  subprocess.call(['heroku', 'run', './manage.py', 'migrate'])
