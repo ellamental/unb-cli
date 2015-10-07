2b4e52e0b6580e04e356fa126352f6bfcb9d61f7
Nick Zarczynski <nick@nickzarr.com>
2015-10-07 03:27:41 -0700

    Add build commands

--- unb_cli/unb/build.py
+++ unb_cli/unb/build.py
@@ -62,7 +62,27 @@ def sphinx_api_docs(component=None):
 @group.command(name='egg')
 def py_egg():
   """Build a Python egg."""
-  subprocess.call(['python', 'setup.py', 'sdist'])
+  subprocess.call(['python', 'setup.py', 'sdist', 'bdist_wheel'])
+
+
+@group.command(name='package')
+@arg('dist', help=("Package version (example: `0.0.3*`).  Use matching to "
+                   "upload multiple versions (source dist and a wheel, for "
+                   "example)."))
+@arg('-r', help=("Repository to upload to.  Common ones include, `pypi` and "
+                 "`testpypi` (they are defined in your `~/.pypirc`)."))
+def package(dist, repo):
+  """Build and upload a Python package.
+
+  Requires [twine](https://pypi.python.org/pypi/twine).
+  """
+  subprocess.call(['python', 'setup.py', 'sdist', 'bdist_wheel'])
+  dist_version = 'dist/' + dist
+  twine_command = ['twine', 'upload', dist_version]
+  if repo:
+     twine_command.append('-r')
+     twine_command.append(repo)
+  subprocess.call(twine_command)
 
 
 # This might not work...
@@ -70,3 +90,8 @@ def py_egg():
 def py_egg_install():
   """Install a Python egg locally (usually during development)."""
   subprocess.call(['pip', 'install', '-e', '.'])
+
+
+@group.command(name='verify-docs')
+def verify_docs():
+  subprocess.call(['rest2html.py', 'README.txt'])
