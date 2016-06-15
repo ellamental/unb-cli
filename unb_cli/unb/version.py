4464b490922e7422b3c8f66fe8be3df5ef02a2e9
Nick Zarczynski <nick@nickzarr.com>
2016-06-15 13:23:46 -0700

    `unb version tag` allow the name to be explicitly given
    Also allow a prefix and/or suffix to be given.

--- unb_cli/unb/version.py
+++ unb_cli/unb/version.py
@@ -13,15 +13,39 @@ version = Command(
 )
 
 
+def _get_version():
+    """Read and return the project version number."""
+    from unb_cli import version
+    v = version.read(current_project().version_file_path)
+    return v or ''
+
+
 def _list_tags():
   """List tags."""
   subprocess.call(['git', 'tag', '-l', '-n'])
 
 
-def _tag(version, message):
-  """Create a git tag."""
-  v = 'v' + version
-  subprocess.call(['git', 'tag', '-a', v, '-m', message])
+def _tag(name, message, prefix='', suffix=''):
+  """Create a git tag.
+
+  Parameters
+  ----------
+  name : str
+      The name of the tag to create
+  message : str
+      A short message about the tag
+  prefix : str
+      A prefix to add to the name
+  suffix : str
+      A suffix to add to the name
+
+  Returns
+  -------
+  None
+
+  """
+  name = prefix + name + suffix
+  subprocess.call(['git', 'tag', '-a', name, '-m', message])
 
 
 def _push_tags():
@@ -38,14 +62,32 @@ def bump(part):
 
 
 @version.register('tag')
-@arg('message', nargs='?', default='')
-def tag(message):
-  """Create a git tag."""
-  from unb_cli import version
-  version_file_path = os.path.join(current_project().path,
-                                   current_project().config.VERSION_FILENAME)
-  v = version.read(version_file_path)
-  _tag(v, message)
+@arg('message', nargs='?', default='',
+     help='Annotate the tag with a message.')
+@arg('--name', nargs='?', default='',
+     help='Specify a tag name explicitly.')
+@arg('--prefix', nargs='?', default='',
+     help="""A prefix to add to the name.
+
+     This is most useful when the name parameter is omitted.  For example, if
+     the current version number were 1.2.3, ``unb version tag --prefix=v``
+     would produce a tag named ``v1.2.3``.""")
+@arg('--suffix', nargs='?', default='',
+     help="""A suffix to add to the name.
+
+     This is most useful when the name parameter is omitted.  For example, if
+     the current version number were 1.2.3, ``unb version tag --suffix=-dev``
+     would produce a tag named ``1.2.3-dev``.""")
+def tag(message, name, prefix, suffix):
+  """Create a git tag.
+
+  If the tag name is not given explicitly, its name will equal the contents of
+  the file project_root/VERSION.
+
+  """
+  if not name:
+      name = _get_version()
+  _tag(name, message, prefix, suffix)
 
 
 @version.register('push-tags')
@@ -63,7 +105,4 @@ def list_tags():
 @version.register('version')
 def get_version():
   """Get the version number of the current project."""
-  from unb_cli import version
-  v = version.read(current_project().version_file_path)
-  if v is not None:
-    print v
+  print _get_version()
