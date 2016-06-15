c0ac841d8dac04a3df5474ffe470bb13c96a54bf
Nick Zarczynski <nick@nickzarr.com>
2016-06-15 13:29:17 -0700

    Begin the switch to 4-space indent

--- unb_cli/unb/version.py
+++ unb_cli/unb/version.py
@@ -7,9 +7,9 @@ from . import current_project
 
 
 version = Command(
-  name='version',
-  title='Utilities for versioning and releases.',
-  description='Utilities for versioning and releases.',
+    name='version',
+    title='Utilities for versioning and releases.',
+    description='Utilities for versioning and releases.',
 )
 
 
@@ -21,44 +21,44 @@ def _get_version():
 
 
 def _list_tags():
-  """List tags."""
-  subprocess.call(['git', 'tag', '-l', '-n'])
+    """List tags."""
+    subprocess.call(['git', 'tag', '-l', '-n'])
 
 
 def _tag(name, message, prefix='', suffix=''):
-  """Create a git tag.
+    """Create a git tag.
 
-  Parameters
-  ----------
-  name : str
-      The name of the tag to create
-  message : str
-      A short message about the tag
-  prefix : str
-      A prefix to add to the name
-  suffix : str
-      A suffix to add to the name
+    Parameters
+    ----------
+    name : str
+        The name of the tag to create
+    message : str
+        A short message about the tag
+    prefix : str
+        A prefix to add to the name
+    suffix : str
+        A suffix to add to the name
 
-  Returns
-  -------
-  None
+    Returns
+    -------
+    None
 
-  """
-  name = prefix + name + suffix
-  subprocess.call(['git', 'tag', '-a', name, '-m', message])
+    """
+    name = prefix + name + suffix
+    subprocess.call(['git', 'tag', '-a', name, '-m', message])
 
 
 def _push_tags():
-  """Run `git push --follow-tags`."""
-  subprocess.call(['git', 'push', '--follow-tags'])
+    """Run `git push --follow-tags`."""
+    subprocess.call(['git', 'push', '--follow-tags'])
 
 
 @version.register('bump')
 @arg('part', nargs='?', default='patch')
 def bump(part):
-  """Bump the version number."""
-  from unb_cli import version
-  version.bump_file(current_project().version_file_path, part, '0.0.0')
+    """Bump the version number."""
+    from unb_cli import version
+    version.bump_file(current_project().version_file_path, part, '0.0.0')
 
 
 @version.register('tag')
@@ -79,30 +79,30 @@ def bump(part):
      the current version number were 1.2.3, ``unb version tag --suffix=-dev``
      would produce a tag named ``1.2.3-dev``.""")
 def tag(message, name, prefix, suffix):
-  """Create a git tag.
+    """Create a git tag.
 
-  If the tag name is not given explicitly, its name will equal the contents of
-  the file project_root/VERSION.
+    If the tag name is not given explicitly, its name will equal the contents of
+    the file project_root/VERSION.
 
-  """
-  if not name:
-      name = _get_version()
-  _tag(name, message, prefix, suffix)
+    """
+    if not name:
+        name = _get_version()
+    _tag(name, message, prefix, suffix)
 
 
 @version.register('push-tags')
 def push_tags():
-  """Push and follow tags.  (`git push --follow-tags`)"""
-  _push_tags()
+    """Push and follow tags.  (`git push --follow-tags`)"""
+    _push_tags()
 
 
 @version.register('list-tags')
 def list_tags():
-  """List git tags."""
-  _list_tags()
+    """List git tags."""
+    _list_tags()
 
 
 @version.register('version')
 def get_version():
-  """Get the version number of the current project."""
-  print _get_version()
+    """Get the version number of the current project."""
+    print _get_version()
